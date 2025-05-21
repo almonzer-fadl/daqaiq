684de2c3340b72a912371814b9f8076162c8a009
almonzerfadl <valmonzer@proton.me>
2025-05-21 22:34:07 +0300

    feat: add internationalization support with next-i18next and email functionality

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ar'],
+  },
+  defaultNS: 'common',
+  localePath: './public/locales',
+}; 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const { i18n } = require('./next-i18next.config');
+
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
 const nextConfig = {
+  i18n,
   images: {
     domains: ['daqaiq.com', 'localhost', 'res.cloudinary.com'],
     remotePatterns: [
